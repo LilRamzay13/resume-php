06473b27e4e54bb96f6b333cc767dec60b0ca388
LilRamzay13 <68799910+LilRamzay13@users.noreply.github.com>
2022-05-16 18:57:22 +0300

    Lil Ramzay
    adaptive+menu-burger

--- elements/header.php
+++ elements/header.php
@@ -1,18 +1,38 @@
 <header>
-    <div class="header">
 
-        <ul class="header__nav">
-            <li><a class="header__link lng-homeln" href="index.php">Главная</a></li>
-            <li><a class="header__link lng-aboutln" href="about.php">Обо мне</a></li>
-            <li><a class="header__link lng-skillsln" href="skills.php">Навыки</a></li>
-            <li><a class="header__link lng-portfolioln" href="portfolio.php">Портфолио</a></li>
-            <select class="change-lang">
-                <option value="ru" selected>RU</option>
-                <option value="en">EN</option>
-            </select>
-        </ul>
+    <div class="wrapper">
+        <input type="checkbox" id="check-menu">
+        <label for="check-menu">MENU</label>
+        <select class="change-lang checked-select">
+            <option value="ru" selected>RU</option>
+            <option value="en">EN</option>
+        </select>
+        <div class="burger-line first"></div>
+        <div class="burger-line second"></div>
+        <div class="burger-line third"></div>
+        <div class="burger-line fourth"></div>
+        <nav class="main-menu">
+        <a class="header__link lng-homeln checked-a" href="index.php">Главная</a>
+        <a class="header__link lng-aboutln checked-a" href="about.php">Обо мне</a>
+        <a class="header__link lng-skillsln checked-a" href="skills.php">Навыки</a>
+        <a class="header__link lng-portfolioln checked-a" href="portfolio.php">Портфолио</a>
+        </nav>
+    </div>
 
-    </div> </br>
+
+    <ul class="header__nav">
+        <li><a class="header__link lng-homeln" href="index.php">Главная</a></li>
+        <li><a class="header__link lng-aboutln" href="about.php">Обо мне</a></li>
+        <li><a class="header__link lng-skillsln" href="skills.php">Навыки</a></li>
+        <li><a class="header__link lng-portfolioln" href="portfolio.php">Портфолио</a></li>
+        <select class="change-lang">
+            <option value="ru" selected>RU</option>
+            <option value="en">EN</option>
+        </select>
+
+    </ul>
+
+    </br>
 
     <div class="outline"></div>
 </header>
